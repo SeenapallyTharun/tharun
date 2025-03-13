1180e745505528158e9b1ed6b305503f255365fa
SeenapallyTharun <tarunseenapally.33@gmail.com>
2025-03-13 05:55:23 +0000

    ff.py

---
+++ ff.py
@@ -0,0 +1,9 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+from google.colab import drive
+drive.mount('/content/drive')
+
